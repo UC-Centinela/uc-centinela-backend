218d4c174375e49eae00dbd52a1f3e5132d87b76
Domingo Agüero <domingo.aguero@uc.cl>
2025-05-22 17:20:38 -0400

    updates de CD

--- docker/prod-up.sh
+++ docker/prod-up.sh
@@ -5,4 +5,4 @@ echo "Copying .env.production to .env..."
 cp .env.production .env
 
 echo "Starting Docker Compose (production)..."
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build 
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build 
