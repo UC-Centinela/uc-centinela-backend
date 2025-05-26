42af3415ad11c9f9fcdd9cb7538e0372e72e0855
Agustín Barriga <agustin717@uc.cl>
2025-05-26 12:00:23 -0400

    feat/implemented tasks by revisor id

--- src/task/infrastructure/task.module.ts
+++ src/task/infrastructure/task.module.ts
@@ -13,6 +13,7 @@ import { DeleteTaskUseCase } from '@task/application/use_cases/delete-task.use-c
 import { FindAllTaskUseCase } from '@task/application/use_cases/find-all-task.use-case'
 import { FindOneTaskUseCase } from '@task/application/use_cases/find-one-task.use-case'
 import { FindByUserIdTaskUseCase } from '@task/application/use_cases/find-by-user-id-task.use-case'
+import { FindByReviewerIdTaskUseCase } from '@task/application/use_cases/find-by-reviewer-id-task.use-case'
 
 @Module({
   imports: [CommonsModule],
@@ -27,6 +28,7 @@ import { FindByUserIdTaskUseCase } from '@task/application/use_cases/find-by-use
     FindAllTaskUseCase,
     FindOneTaskUseCase,
     FindByUserIdTaskUseCase,
+    FindByReviewerIdTaskUseCase,
 
     {
       provide: 'ITaskService',
