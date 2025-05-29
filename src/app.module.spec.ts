9857ecdcab52b96fe83ae469dc9096cf8e2df32f
Agustín Barriga <agustin717@uc.cl>
2025-05-29 13:08:03 -0400

    fix: app.module.spec fixed with mock try

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from '@nestjs/testing'
 import { AppModule } from './app.module'
+import { IBMStorageAdapter } from './multimedia/infrastructure/adapters/ibm-storage.adapter'
 
 describe('AppModule', () => {
   it('should compile without errors', async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     })
-      .overrideProvider('IBMStorageAdapter')
+      .overrideProvider(IBMStorageAdapter)
       .useValue({})
       .compile()
 
